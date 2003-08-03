82515a1ac010641024195cee640933b5b1bc7097
Randal L. Schwartz <merlyn@stonehenge.com>
2003-08-03 00:12:47 +0000

    Import of MERLYN/Acme-Current-20030803 from CPAN.
    gitpan-cpan-distribution: Acme-Current
    gitpan-cpan-version:      20030803
    gitpan-cpan-path:         MERLYN/Acme-Current-20030803.tar.gz
    gitpan-cpan-author:       MERLYN
    gitpan-cpan-maturity:     released

--- lib/Acme/Current.pm
+++ lib/Acme/Current.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION);
 
 use vars qw($YEAR $MONTH $DAY);
 
-$VERSION = sprintf "%04d%02d%02d", $YEAR = 2003, $MONTH = 8, $DAY = 2;
+$VERSION = sprintf "%04d%02d%02d", $YEAR = 2003, $MONTH = 8, $DAY = 3;
 
 1;
 
